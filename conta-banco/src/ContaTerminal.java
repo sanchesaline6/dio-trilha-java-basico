3a0c35793b6d14560b38070f00650bf82ce786a6
Aline Sanches <sanches.aline6@gmail.com>
2024-05-14 21:53:52 -0300

    Implementa ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,20 +2,27 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-
     public static void main(String[] args) {
+
         int numero;
         String agencia;
         String nomeCliente;
         double saldo;
 
-        System.out.println("Por favor, digite o número da agência!");
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da agência: ");
         agencia = sc.nextLine();
-        numero = sc.nextInt();
+
+        System.out.print("Qual é o seu nome? ");
         nomeCliente = sc.nextLine();
+
+        System.out.print("Agora, nos informe o número da sua conta: ");
+        numero = sc.nextInt();
+
+        System.out.print("Por fim, nos informe seu saldo: ");
         saldo = sc.nextDouble();
 
-        System.out.print("Olá" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é" + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
